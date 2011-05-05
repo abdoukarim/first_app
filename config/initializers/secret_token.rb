0252034286c5bbfd30d5ae62311dd7ddd236b7fe
root <root@kage-Satellite-A505>
2011-05-05 23:02:12 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '734cf9a74b3cd91c5d56796b62915b4b1e1b1622d8f3b82a289a7ab3123ea735693213702e3975c76a2682d4b3b7634d3317b225d0b22c5389f01367dc20ae74'
